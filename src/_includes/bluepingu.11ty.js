2017e2f4a7951dadedc239b89594170c09b2f5b9
Alexander Wunschik <alex@wunschik.net>
2025-12-30 21:27:17 +0100

    update to new bluepingu design

--- src/_includes/bluepingu.11ty.js
+++ src/_includes/bluepingu.11ty.js
@@ -4,7 +4,7 @@ var absolutify = require('absolutify');
 const cheerio = require('cheerio');
 
 const template = './tmp/werkstadt-2031.html';
-const templateUrl = 'https://fuerth.bluepingu.de/werkstadt-2031/';
+const templateUrl = 'https://werkstadt-2031.bluepingu.de/';
 
 /* https://stackoverflow.com/a/36555927 */
 function replaceAll(str, find, replace) {
@@ -49,45 +49,66 @@ module.exports = async function(data) {
 	const templateUrlBase = (new URL(templateUrl)).origin
 	let parsed = absolutify(html, templateUrlBase);
 
-	// sadly absolutify breaks the glyphicons. Lets fix this!
-	parsed = replaceAll(parsed, `xlink:href="${templateUrlBase}/#`, 'xlink:href="#');
-
 	// remove unnecessary links to reduce loading time
 	[
-		'<link rel="stylesheet" type="text/css" href="https://fuerth.bluepingu.de/typo3conf/ext/def_cookieconsent/Resources/Public/Css/cookieconsent.css?1626256420" media="all">',
-		'<link rel="stylesheet" type="text/css" href="https://cdn.bluepingu.de/tooltipster/4.2.8/tooltipster.css" media="all">',
-		'<link rel="stylesheet" type="text/css" href="https://cdn.bluepingu.de/fancybox/3.5.7/fancybox.css" media="all">',
-		'<link rel="stylesheet" type="text/css" href="https://cdn.bluepingu.de/masterslider/2.81.9/style/masterslider.css" media="all">',
-		'<link rel="stylesheet" type="text/css" href="https://cdn.bluepingu.de/masterslider/2.81.9/skins/minimal/style.css" media="all">',
-		'<link rel="stylesheet" type="text/css" href="https://cdn.bluepingu.de/swiper/6.7.0/swiper-bundle.css" media="all">',
-		'<link rel="stylesheet" type="text/css" href="https://cdn.bluepingu.de/plyr/3.6.3/plyr.css" media="all"></link>',
-		'<script src="https://cdn.bluepingu.de/jquery/3.6.0/jquery-3.6.0.min.js"></script>',
-		'<script src="https://fuerth.bluepingu.de/typo3conf/ext/def_cookieconsent/Resources/Public/JavaScript/cookieconsent.js?1620639799"></script>',
-		'<script src="https://fuerth.bluepingu.de/typo3conf/ext/powermail/Resources/Public/JavaScript/Powermail/Form.min.js?1661243542"></script>',
-		'<script src="https://cdn.bluepingu.de/parsley/2.9.2/parsley.min.js"></script>',
-		'<script src="https://cdn.bluepingu.de/smoothscroll/2.2.0/jquery.smoothscroll.min.js"></script>',
-		'<script src="https://cdn.bluepingu.de/zebrapin/2.0.0/zebrapin.min.js"></script>',
-		'<script src="https://cdn.bluepingu.de/tooltipster/4.2.8/tooltipster.min.js"></script>',
-		'<script src="https://cdn.bluepingu.de/fancybox/3.5.7/jquery.fancybox.min.js"></script>',
-		'<script src="https://cdn.bluepingu.de/masterslider/2.81.9/masterslider.min.js"></script>',
-		'<script src="https://cdn.bluepingu.de/swiper/6.7.0/swiper-bundle.min.js"></script>',
-		'<script src="https://cdn.bluepingu.de/plyr/3.6.3/plyr.min.js"></script>',
-		'<script src="https://cdn.bluepingu.de/bulma-calendar/6.0.8/bulma-calendar.min.js"></script>',
-		'<script src="https://fuerth.bluepingu.de/typo3conf/ext/def_package/Resources/Public/JavaScript/jquery.minimal.js?1656596780"></script>',
-		'<script src="https://fuerth.bluepingu.de/typo3conf/ext/def_package/Resources/Public/JavaScript/jquery.www.js?1650970500"></script>',
-		'<script src="https://fuerth.bluepingu.de/typo3conf/ext/def_package/Resources/Public/JavaScript/jquery.forms.js?1643020119"></script>',
+		`<link rel="profile" href="https://gmpg.org/xfn/11">`,
+		`<link rel="alternate" type="application/rss+xml" title="Werkstatt Fürth 2031 » Feed" href="https://werkstadt-2031.bluepingu.de/feed/">`,
+		`<link rel="alternate" type="application/rss+xml" title="Werkstatt Fürth 2031 » Kommentar‑Feed" href="https://werkstadt-2031.bluepingu.de/comments/feed/">`,
+		`<link rel="alternate" type="application/rss+xml" title="Werkstadt Fürth 2031 – Werkstadt Fürth 2031 – Nachfragen.Umdenken.Mitwirken.-Kommentar‑Feed" href="https://werkstadt-2031.bluepingu.de/beispiel-seite/feed/">`,
+		`<link rel="icon" href="https://werkstadt-2031.bluepingu.de/wp-content/uploads/sites/12/2025/02/cropped-Bluepingu_Logo_2024_farbe_quadrat-32x32.png" sizes="32x32" />`,
+		`<link rel="icon" href="https://werkstadt-2031.bluepingu.de/wp-content/uploads/sites/12/2025/02/cropped-Bluepingu_Logo_2024_farbe_quadrat-192x192.png" sizes="192x192" />`,
+		`<link rel="apple-touch-icon" href="https://werkstadt-2031.bluepingu.de/wp-content/uploads/sites/12/2025/02/cropped-Bluepingu_Logo_2024_farbe_quadrat-180x180.png" />`,
+		`<link rel="https://api.w.org/" href="https://werkstadt-2031.bluepingu.de/wp-json/" />`,
+		`<link rel="alternate" title="JSON" type="application/json" href="https://werkstadt-2031.bluepingu.de/wp-json/wp/v2/pages/2" />`,
+		`<link rel="EditURI" type="application/rsd+xml" title="RSD" href="https://werkstadt-2031.bluepingu.de/xmlrpc.php?rsd" />`,
+		`<link rel="canonical" href="https://werkstadt-2031.bluepingu.de/" />`,
+		`<link rel='shortlink' href='https://werkstadt-2031.bluepingu.de/' />`,
+		`<figure class="wp-block-image alignleft size-full is-resized"><img decoding="async" src="https://umbau.werkstatt-fuerth-2031.bluepingu.de/wp-content/uploads/sites/12/2025/02/Bluepingu_Logo_2024_sw_svg.svg" alt="" class="wp-image-66" style="width:75px;height:auto"></figure>`,
 	].forEach(scriptTag => {
-		parsed = parsed.replace(scriptTag, `<!-- ${scriptTag} -->`);
+		parsed = replaceAll(parsed, scriptTag, `<!-- content removed -->`);
 	});
 
 	const $ = cheerio.load(parsed);
 
-	// remove the cookie popup (not needed here)
-	$('#def-cookieconsent-consent-popup').remove();
-	$('.page-title-link.def-cookieconsent-popup-toggle').parent().parent().remove();
+	// cleanup
+	[
+		'link#animate-css-css',
+		'link#ct-page-title-styles-css',
+		'link#ct-stackable-styles-css',
+		'link#ics-calendar-css',
+		'link#parent-style-css',
+		'link#ct-page-title-styles-css',
+		'link#ct-stackable-styles-css',
+		'link#ics-calendar-css',
+		
+		'script#real-cookie-banner-banner-js',
+		'script#real-cookie-banner-blocker-js',
+		'script#real-cookie-banner-vendor-real-cookie-banner-banner-js',
+		'script#jquery-core-js',
+		'script#jquery-migrate-js',
+		'script#ct-scripts-js',
+		'script#real-cookie-banner-banner-js-before',
+		
+		'noscript',
+		'style.stk-block-styles',
+		'style#ugb-style-css-inline-css',
+		'style#ugb-style-css-nodep-inline-css',
+		'#ic-calendar-js',
+		'#ics-calendar-js',
+		'.wp-block-column',
+		'#ics-calendar-js-after',
+		'#ct-scripts-js-extra',
+		'#core-block-supports-inline-css',
+		'script[type="speculationrules"]',
+		'script[data-skip-lazy-load="js-extra"]',
+		'link[rel="alternate"]',
+		'link[rel="preload"]',
+	].forEach(selector => {
+		$(selector).remove();
+	})
 
 	// replace content with the content of the template
-	$('#content .container').html(data.content);
+	$('.entry-content').html(data.content);
 	
 	const cleanHtml = $.html();
 
